a831bc3511b9bf9e49f20903202c229943e16c07
kanary <kanary159357@gmail.com>
2021-01-05 16:26:51 +0900

    icon, gifParse

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
+
     var xOffset = 10;
     var yOffset = 30;
     $("img:regex(src, .*\.gif$)").hover(function(e){
-        console.log($(this).attr('src'));
-        //if((/.*\.gif/.test($(this).attr('src')))){
-             
+        console.log($(this).attr('src'));             
         $("body").append("<p id='preview'><img src='"+$(this).attr('src') +"' width='400px' /></p>");								 
         $("#preview")
             .css("top",(e.pageY - xOffset) + "px")
             .css("left",(e.pageX + yOffset) + "px")
             .fadeIn("fast");	
-       // }
     }, 
     function(){
         $("#preview").remove()
@@ -25,6 +23,3 @@ $(document).ready(function(){
     );
 
 });
-
-
-
